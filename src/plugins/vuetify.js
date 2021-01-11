38cf16f1fac4d9ec82753a38d235b001e7cfb0cb
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-01-11 22:54:00 +0100

    dark theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,7 +5,10 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     iconfont:'md',
+    theme: { 
+      dark: true,
+    },
     icons: {
-        iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
-      },
+      iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="fill-height" fluid>
+      <v-container class="fill-height pa-16" fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="8">
             <v-card class="elevation-12">
@@ -10,31 +10,30 @@
                   <v-row>
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Sign in to Jetlight</h1>
+                        <h1 class="text-center display-2 white--text font-weight-bold">Sign in to Jetlight</h1>
                         <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>facebook</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>mdi-google</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>mdi-linkedin</v-icon>
                           </v-btn>
                         </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
-                          <v-text-field label="Email" name="Email" prepend-icon="email" type="email" color="teal accent-3"/>
-                          <v-text-field id="password" label="Password" name="Password" prepend-icon="lock" type="password" color="teal accent-3"/>
+                        <v-form class="mt-4">
+                          <v-text-field label="Email" name="Email" prepend-icon="email" type="email" color="#01579B"></v-text-field>
+                          <v-text-field id="password" label="Password" name="Password" prepend-icon="lock" type="password" color="#01579B"></v-text-field>
                         </v-form>
                         <h3 class="text-center mt-3">Forget your password ?</h3>
                       </v-card-text>
                       <div class="text-center mt-3">
-                        <v-btn @click="login" rounded dark class="teal accent-3 mb-3">SIGN IN</v-btn>
+                        <v-btn @click="login" rounded dark class="mb-3" style="background: #082543;">SIGN IN</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
+                    <v-col cols="12" md="4"  style="background: #082543;">
+                      <v-card-text class="white--text mt-16 pt-16">
                         <h1 class="text-center display-1">Hello, Friends !</h1>
                         <h5 class="text-center">Enter your personnel details and start journay with us</h5>
                       </v-card-text>
@@ -46,8 +45,8 @@
                 </v-window-item>
                 <v-window-item :value="2">
                   <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
+                    <v-col cols="12" md="4" style="background: #082543;">
+                      <v-card-text class="white--text mt-16 pt-16">
                         <h1 class="text-center display-1">Welcome Back</h1>
                         <h5 class="text-center">To Keep connected with us please login with your personel info</h5>
                       </v-card-text>
@@ -57,27 +56,26 @@
                     </v-col>
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <h1 class="text-center display-2 white--text font-weight-bold">Create Account</h1>
                         <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>facebook</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>mdi-google</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>mdi-linkedin</v-icon>
                           </v-btn>
                         </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
-                          <v-text-field label="Name" name="Name" prepend-icon="person" type="text" color="teal accent-3" />
-                          <v-text-field label="Email" name="Email" prepend-icon="email" type="email" color="teal accent-3" />
-                          <v-text-field label="Password" name="Password" prepend-icon="lock" type="password" color="teal accent-3"/>
+                        <v-form class="mt-4">
+                          <v-text-field label="Name" name="Name" prepend-icon="person" type="text" color="#01579B" ></v-text-field>
+                          <v-text-field label="Email" name="Email" prepend-icon="email" type="email" color="#01579B" ></v-text-field>
+                          <v-text-field label="Password" name="Password" prepend-icon="lock" type="password" color="#01579B"></v-text-field>
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-n5">
-                        <v-btn @click="signUp" rounded dark class="teal accent-3 mb-3">SIGN UP</v-btn>
+                        <v-btn @click="signUp" rounded dark class=" mb-3" style="background: #082543;">SIGN UP</v-btn>
                       </div>
                     </v-col>
                   </v-row>
